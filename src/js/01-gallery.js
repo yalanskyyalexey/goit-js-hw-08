0300a8d4e21270d41741dd292f4f9bf9ddc8a6fa
yalanskyyalexey <yalanskyyalexey@gmail.com>
2021-10-05 15:45:25 +0300

    remove changes

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,7 +2,7 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-//console.log(galleryItems);
+console.log(galleryItems);
 const newGallery = document.querySelector('.gallery');
 
 const markup = galleryItems
